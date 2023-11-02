60004a88c1940d9bd384d3d9b9c9cf774ee8d383
Emamul Islam Rabbi <54804110+Rabbi-ProUser@users.noreply.github.com>
2023-11-02 13:43:27 +0600

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,25 @@
+// Menu Hide Show
+var div = document.getElementById('drop-menu');
+var display = 0;
+
+
+function hideShow()
+{
+	if(display == 1)
+	{
+		div.style.display = 'flex';
+		display = 0;
+	}
+
+	else
+
+	{
+		div.style.display = 'none';
+		display = 1;
+	}
+}
+
+
+
+// SECOUND SLIDER
+
